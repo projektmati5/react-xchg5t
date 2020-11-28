cde9492134bdb1a1181b4a67947ea86751634298
projektmati5 <mateuszprojekcik5@protonmail.com>
2020-11-28 22:54:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-xchg5t
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xchg5t)
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+
+ReactDOM.render(
+  <h1>Witaj, świecie!</h1>,
+  document.getElementById('app')
+);
+
+
+//ReactDOM.render(<App />, document.getElementById("root"));
