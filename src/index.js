5a71a33adf0086721b097a818f80692c34933326
projektmati5 <mateuszprojekcik5@protonmail.com>
2020-11-29 16:44:02 +0100

    1 file modified

--- src/index.js
+++ src/index.js
@@ -4,7 +4,29 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {counter: 0};
+  }
+  increment() {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
 
+  render() {
+    return (
+
+      
+      <div>
+        <button onClick={this.increment}>+</button>
+        <output>{this.state.counter}</output>
+        <button>-</button>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
  <App/>,
