5804a01c4a9e57d9b722a85cfa328a9400b5f574
projektmati5 <mateuszprojekcik5@protonmail.com>
2020-11-29 22:35:15 +0100

    1 file modified

--- src/index.js
+++ src/index.js
@@ -4,48 +4,53 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 
-class App extends React.Component{
-constructor(){
-  super();
-this.state={
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-      sumCount: 0,
-      totalCount: 0,
-      doubleClickCount: 0}
-};
+    this.state = { dateVisible: true };
+  }
 
-render()
-{return(
-  <div><button onClick={this.increment.bind(this)}>+</button>
-   <output>Suma: {this.state.sumCount}</output></div>
+  onButtonClick() {
+    this.setState(state => ({ dateVisible: !state.dateVisible }));
+  }
 
-
-
-
-);
-
-
+  render() {
+    return (
+      <div>
+        <button onClick={this.onButtonClick.bind(this)}>Kliknij</button>
+        {this.state.dateVisible && <DateComponent />}
+      </div>
+    );
+  }
 }
 
+class DateComponent extends React.Component {
+  constructor(props) {
+    super(props);
 
-increment()
-{
-  this.setState(
-    {
-      sumCount:this.state.sumCount+1,
-      totalCount:this.state.totalCount+1
-      }
-      )
-   
+    this.state = {
+      date: new Date()
+    };
+  }
+
+  componentDidMount() {
+    this.timerId = window.setInterval(this.updateDate.bind(this), 1000);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  updateDate() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    const dateStr = this.state.date.toString();
+    return <time>{dateStr}</time>;
+  }
 }
-
-
-}
-
-
-
-
-
-
-
 ReactDOM.render(<App />, document.getElementById("app"));
