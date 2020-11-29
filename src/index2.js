562aa52c382118864a861013e07128b13cdf2e43
projektmati5 <mateuszprojekcik5@protonmail.com>
2020-11-29 21:19:39 +0100

    1 file modified, 1 added

---
+++ src/index2.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      sumCount: 0,
+      totalCount: 0,
+      doubleClickCount: 0
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        <button onClick={this.increment.bind(this)}>+</button>
+        <div onDoubleClick={this.doubleClick.bind(this)}>
+          <output>Suma: {this.state.sumCount}</output>
+          <output>Kliknięcia: {this.state.totalCount}</output>
+          <output>Podwójne kliknięcia: {this.state.doubleClickCount}</output>
+        </div>
+        <button onClick={this.decrement.bind(this)}>-</button>
+      </div>
+    );
+  }
+  
+  increment() {
+    this.setState({
+      sumCount: this.state.sumCount + 1,
+      totalCount: this.state.totalCount + 1
+    })
+  }
+  
+  decrement() {
+    this.setState({
+      sumCount: this.state.sumCount - 1,
+      totalCount: this.state.totalCount + 1
+    })
+  }
+  
+  doubleClick() {
+    this.setState({
+      doubleClickCount: this.state.doubleClickCount+ 1
+    });
+  }
+}
+ReactDOM.render(<App />, document.getElementById("app"));
