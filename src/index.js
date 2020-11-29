673f297721c1406e2786771d46338ad24a526f89
projektmati5 <mateuszprojekcik5@protonmail.com>
2020-11-29 16:57:15 +0100

    1 file modified

--- src/index.js
+++ src/index.js
@@ -7,28 +7,45 @@ import App from "./App";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {counter: 0};
+    this.state = {
+      sumCount: 0,
+      totalCount: 0,
+      doubleClickCount: 0
+    };
   }
-  increment() {
-    this.setState({
-      counter: this.state.counter + 1
-    })
-  }
-
+  
   render() {
     return (
-
-      
       <div>
         <button onClick={this.increment.bind(this)}>+</button>
-        <output>{this.state.counter}</output>
-        <button>-</button>
+        <div onDoubleClick={this.doubleClick.bind(this)}>
+          <output>Suma: {this.state.sumCount}</output>
+          <output>Kliknięcia: {this.state.totalCount}</output>
+          <output>Podwójne kliknięcia: {this.state.doubleClickCount}</output>
+        </div>
+        <button onClick={this.decrement.bind(this)}>-</button>
       </div>
     );
   }
+  
+  increment() {
+    this.setState({
+      sumCount: this.state.sumCount + 1,
+      totalCount: this.state.totalCount + 1
+    })
+  }
+  
+  decrement() {
+    this.setState({
+      sumCount: this.state.sumCount - 1,
+      totalCount: this.state.totalCount + 1
+    })
+  }
+  
+  doubleClick() {
+    this.setState({
+      doubleClickCount: this.state.doubleClickCount+ 1
+    });
+  }
 }
-
-ReactDOM.render(
- <App/>,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById("app"));
