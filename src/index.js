5418202afe2536602152ca3d53e46bdb17895308
projektmati5 <mateuszprojekcik5@protonmail.com>
2020-11-29 21:45:53 +0100

    1 file modified

--- src/index.js
+++ src/index.js
@@ -3,4 +3,27 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
+
+class App extends React.Component{
+constructor(){
+  super();
+this.state={
+
+      sumCount: 0,
+      totalCount: 0,
+      doubleClickCount: 0}
+};
+
+render()
+{return(null);
+}
+
+}
+
+
+
+
+
+
+
 ReactDOM.render(<App />, document.getElementById("app"));
